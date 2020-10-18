4c8e5760eff33af1408c616a29fc7500d17bac6d
Wayne Yang <wyang@netflix.com>
2020-10-17 21:03:36 -0700

    Initial vesrion of Quora dataset class

---
+++ data_utils.py
@@ -0,0 +1,129 @@
+from collections import defaultdict, namedtuple
+from itertools import chain
+import random
+from typing import List
+import numpy as np
+
+Example = namedtuple("Example",
+                     ["id", "qid1", "qid2", "q1_text", "q2_text", "is_duplicate"])
+
+
+def generate_data(examples: List[Example], reachable):
+    """
+    Use the transitive closure of the graph to generate all relevant
+    pairs of qids.
+
+    Args:
+        examples:
+            A list of positive examples
+        reachable:
+            A dict of qid to its reachable nodes
+
+    Returns:
+        A set of tuples of (qid1, qid2) which are relevant
+    """
+    result = []
+    for e in examples:
+        for neighbor in reachable[e.qid1]:
+            result.append((e.qid1, neighbor))
+        for neighbor in reachable[e.qid2]:
+            result.append((e.qid2, neighbor))
+    return set(result)
+
+
+def build_graph(examples: List[Example]):
+    """
+    Build an adjacent lists based on the examples.  Each node
+    is represented by the qid of a question.
+
+    Args:
+        examples: A list of Examples to build adjacent lists on.
+
+    Returns:
+        Returns a dict of qid to its adjacent nodes
+    """
+    # graph is an adjacency list
+    graph = defaultdict(list)
+    for ex in examples:
+        graph[ex.qid1].append(ex.qid2)
+        graph[ex.qid2].append(ex.qid1)
+    return graph
+
+
+def split_examples(examples: List[Example], split_fracs: List[float], seed=None):
+    """
+    Shuffle the examples and then split the examples according to split fractions.
+
+    Args:
+        examples:
+            A list of Examples to split
+        split_fracs:
+            A list of fractions denoting proportions that must add up to 1
+        seed:
+            Seed for random shuffling
+
+    Returns:
+        A list of lists of Examples that correspond to the split fractions.
+    """
+
+    if seed:
+        random.seed(seed)
+    random.shuffle(examples)
+
+    split_points = [0] + [int(round(frac * len(examples)))
+                          for frac in np.cumsum(split_fracs)]
+
+    return [examples[split_points[i]:split_points[i + 1]]
+            for i in range(len(split_fracs))]
+
+
+def build_transitive_closure(graph):
+    """
+    Build a transitive closure based on adjacent lists
+
+    Args:
+        graph:
+            A dict of qid to its adjacent nodes in the graph
+
+    Returns:
+        A dict of qid to a set of nodes reachable from it
+    """
+    reachable = defaultdict(set)
+    for u in graph.keys():
+        dfs(graph, u, u, reachable)
+    return reachable
+
+
+def dfs(graph, u, v, reachable):
+    """
+    Depth-first search traversal of graph to build the transitive
+    closure
+
+    Args:
+        u:
+            Current node
+        v:
+            A node reachable from u
+        reachable:
+            A dict of qid to a set of nodes reachable from it
+
+    """
+    if u != v:
+        # exclude itself
+        reachable[u].add(v)
+    for w in graph[v]:
+        if w not in reachable[u]:
+            dfs(graph, u, w, reachable)
+
+
+def flatmap(iterable):
+    """
+    Short-hand for [(a,b), (c, d, e)] -> [a, b, c, d, e]
+
+    Args:
+        iterable:
+            An object that is iterable
+    Returns:
+        A list that is flattened by one-level
+    """
+    return list(chain.from_iterable(iterable))
---
+++ quora_dataset.py
@@ -0,0 +1,96 @@
+from data_utils import *
+
+
+class QuoraDataset:
+    """
+    A data class for the Quora dataset at:
+
+    https://www.quora.com/q/quoradata/First-Quora-Dataset-Release-Question-Pairs
+    """
+    def __init__(self, src_filename, split_fracs=[0.03, 0.97], seed=1):
+        """
+            Args:
+                src_filename:
+                    Local file path to the Quora Question Dataset
+        """
+        # 404279 examples,  149263 positive
+        examples = list(self.read_examples(src_filename))
+        qid2text = {
+            qid: text for qid, text in
+                     flatmap([[(e.qid1, e.q1_text), (e.qid2, e.q2_text)] for e in examples])
+        }
+
+        pos_examples = [e for e in examples if e.is_duplicate]
+
+        # Build adjacency list
+        graph = build_graph(pos_examples)
+
+        # Build reachability map
+        reachable = build_transitive_closure(graph)
+
+        # Only use pos_examples because negative sampling would be used
+        test_examples, train_examples = split_examples(pos_examples, split_fracs, seed=seed)
+
+        test_data = generate_data(test_examples, reachable)
+
+        # Each element in a positive pair is considered a test query
+        test_qid_set = set(flatmap(test_data))
+
+        # Filter out training examples that have qid appearing in test_query
+        train_examples = [e for e in train_examples
+                          if e.qid1 not in test_qid_set or e.qid2 not in test_qid_set]
+
+        train_data = generate_data(train_examples, reachable)
+
+        self._qid2text = qid2text
+        self._examples = examples
+        self._reachable = reachable
+        self._test_data = test_data
+        self._train_data = train_data
+
+    def get_examples(self):
+        return self._examples
+
+    def get_query_result(self, query):
+        return self._reachable.get(query, [])
+
+    def get_text_for_qid(self, qid):
+        return self._qid2text.get(qid, None)
+
+    def get_train_data(self):
+        return self._train_data
+
+    def get_test_data(self):
+        return self._test_data
+
+    @staticmethod
+    def read_examples(src_filename):
+        """
+        Iterator for the Quora question dataset.
+
+        Args:
+            src_filename:
+                Full path to the file to be read.
+        """
+        with open(src_filename, encoding="utf8") as f:
+            # Skip header line
+            next(f)
+
+            for line in f:
+                fields = line.strip().split("\t")
+
+                # Throw away malformed lines (e.g. containing "\n")
+                if len(fields) != 6:
+                    continue
+
+                # convert is_duplicate to boolean
+                fields[-1] = True if fields[-1] == "1" else False
+
+                yield Example(*fields)
+
+
+if __name__ == "__main__":
+    quora = QuoraDataset("quora_duplicate_questions.tsv")
+    test_data = quora.get_test_data()
+    print(test_data)
+
