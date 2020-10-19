5e8e32ad8a711e1d3e3f0e21dfb74f2eacc60072
Adam Zieliński <adam@adamziel.com>
2020-10-19 23:58:27 +0200

    Refactor create_embeddings to compute_bert_embeddings

--- model.py
+++ model.py
@@ -1,13 +1,13 @@
 from embeddings import BERTWrapper, BERTVersion, average_layers_and_tokens, embeddings_factory
 
 bert = BERTWrapper(BERTVersion.BERT_BASE_UNCASED)
-create_embeddings = embeddings_factory(bert, average_layers_and_tokens)
+compute_bert_embeddings = embeddings_factory(bert, average_layers_and_tokens)
 
-embeddings = create_embeddings([
+bert_embeddings = compute_bert_embeddings([
     "We plan to apply the feature-based approach with BERT (Devlin et al. 2018) ",
     "By extracting the final layer’s activations without updating.",
     "We would average.",
     "This sequence of hidden states for the whole input sequence to summarize the semantic content of the input."
 ])
 
-print(embeddings.shape)
+print(bert_embeddings.shape)
