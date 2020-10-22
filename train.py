6ad3b56421d2aca01296839adf4bfdf79d5c2f66
Adam Zieliński <adam@adamziel.com>
2020-10-23 00:19:53 +0200

    Use quora dataset for training

--- train.py
+++ train.py
@@ -1,26 +1,29 @@
 from models import BERTAsFeatureExtractorEncoder, BERTVersion
 from torch import optim
 from train_utils import fit
+from quora_dataset import QuoraDataset
+from loggers import getLogger
 
+logger = getLogger(__name__)
 
-train_data = [
-    ("What is the greatest mystery in the universe?", "I would like to know about universe's mysteries."),
-    ("What is the greatest mystery in the universe?", "What do you think is the universe's greatest mystery?"),
-]
+logger.info("Loading Quora dataset...")
+dataset = QuoraDataset(limit=100)
+logger.info("Quora dataset loaded")
 
-test_data = [
-    ("What is square root of 9?", "I would like to know the square root of 9."),
-    ("What is square root of 9?", "Anyone knows square root of 9?"),
-]
+train_data = dataset.get_train_data()
+test_data = dataset.get_test_data()
 
 learning_rate = 1e-1
 model = BERTAsFeatureExtractorEncoder(BERTVersion.BASE_UNCASED)
+# @TODO: Change to a different optimizer
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+logger.info("Running fit()")
 fit(
     epochs=5,
     model=model,
     opt=optimizer,
     train_data=train_data,
     test_data=test_data,
-    candidates=("square", "root")
+    candidates=("square", "root"),
+    batch_size=10,
 )
