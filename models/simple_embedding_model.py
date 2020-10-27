0784594cfcfd47c9165c6480efe5e993be4d613c
Wayne Yang <wyang@netflix.com>
2020-10-27 16:16:52 +0000

    Add cuda support to baseline model

--- models/simple_embedding_model.py
+++ models/simple_embedding_model.py
@@ -3,14 +3,22 @@ import torch
 from torch import nn
 from transformers import AutoTokenizer
 
+from loggers import getLogger
+
+logger = getLogger(__name__)
+
 
 class SimpleEmbeddingModel(nn.Module):
-    def __init__(self, embedding_dim, pretrained_tokenizer_name='bert-base-uncased'):
+    def __init__(self, embedding_dim, pretrained_tokenizer_name='bert-base-uncased', device=None):
         super(SimpleEmbeddingModel, self).__init__()
+        self.device = device
+        if not self.device:
+            self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
         self.tokenizer = AutoTokenizer.from_pretrained(pretrained_tokenizer_name)
         self.embeddings = nn.Embedding(self.tokenizer.vocab_size,
                                        embedding_dim,
-                                       self.tokenizer.pad_token_id)
+                                       self.tokenizer.pad_token_id).to(self.device)
 
 
     def forward(self, inputs: List[str]):
@@ -31,7 +39,7 @@ class SimpleEmbeddingModel(nn.Module):
 
         # encoded_input.input_ids: (batch_size, max_word_count)
         # embedding: (batch_size, max_word_count, embedding_dim)
-        embedding = self.embeddings(encoded_input.input_ids)
+        embedding = self.embeddings(encoded_input.input_ids.to(self.device))
         return torch.mean(embedding, dim=1)
 
 
